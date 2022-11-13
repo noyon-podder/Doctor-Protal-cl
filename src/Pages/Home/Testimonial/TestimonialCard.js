0789ae4335f2850d317cf06655255d738d207f64
noyon-podder <noyon.vai369@gmail.com>
2022-11-13 10:50:39 +0600

    home page completed

--- src/Pages/Home/Testimonial/TestimonialCard.js
+++ src/Pages/Home/Testimonial/TestimonialCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TestimonialCard = ({ testimonial }) => {
   const { author, profile, address, review } = testimonial;
-  return <div className="card shadow-xl p-5">
+  return <div className="card shadow-xl py-10 px-5">
     <p className="mb-4">{review}</p>
     <div className="flex gap-4 mt-6 items-center">
     <div className="w-16 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
